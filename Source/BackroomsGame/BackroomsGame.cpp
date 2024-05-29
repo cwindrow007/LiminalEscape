4e4050b08a512d029e869809a45c7db9a2b97e5a
Chris <ChristoWindow@gmail.com>
2024-05-29 12:34:41 -0400

    AddedC++

---
+++ Source/BackroomsGame/BackroomsGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BackroomsGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BackroomsGame, "BackroomsGame" );
