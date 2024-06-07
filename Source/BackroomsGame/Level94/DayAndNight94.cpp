989d85addbfc3de3a6906c5f07244692c46c254a
Chris <ChristoWindow@gmail.com>
2024-06-07 10:07:31 -0400

    PossibleNewDayAndNightCycle

---
+++ Source/BackroomsGame/Level94/DayAndNight94.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DayAndNight94.h"
+
+// Sets default values
+ADayAndNight94::ADayAndNight94()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADayAndNight94::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADayAndNight94::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
