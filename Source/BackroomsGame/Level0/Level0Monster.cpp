d30a3e7fea7f8f42b7420749de2132d1b55bd0d2
Chris <ChristoWindow@gmail.com>
2024-06-25 22:23:45 -0400

    8 hour shift with 7 breaks over.

--- Source/BackroomsGame/Level0/Level0Monster.cpp
+++ Source/BackroomsGame/Level0/Level0Monster.cpp
@@ -1,5 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+//This animation is broken and needs a fix do this tmr.
+
 #include "Level0Monster.h"
 
 #include "AIController.h"
