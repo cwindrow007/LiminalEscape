598b8493709356f13c265958e95d59f2e57f7598
Chris <ChristoWindow@gmail.com>
2024-07-09 16:01:58 -0400

    title screen prep + Character rework

---
+++ Source/BackroomsGame/Menus/GameSettingsManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "GameSettingsManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BACKROOMSGAME_API UGameSettingsManager : public UObject
+{
+	GENERATED_BODY()
+	
+};
