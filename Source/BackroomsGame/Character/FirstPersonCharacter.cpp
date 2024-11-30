7bdea58f21f218b7e3c5d7670f6f3b6bb561b342
Chris <ChristoWindow@gmail.com>
2024-11-29 23:23:15 -0500

    Changes in Code monster work Tomorrow

--- Source/BackroomsGame/Character/FirstPersonCharacter.cpp
+++ Source/BackroomsGame/Character/FirstPersonCharacter.cpp
@@ -168,7 +168,9 @@ void AFirstPersonCharacter::PerformInteractionCheck()
 
     if(LookDirection > 0)
     {
-        DrawDebugLine(GetWorld(), TraceStart, TraceEnd, FColor::Red, false, 1.0f, 0, 2.0f);
+        //!This is the line debugger incase you want to check item checks
+        //! 
+       // DrawDebugLine(GetWorld(), TraceStart, TraceEnd, FColor::Red, false, 1.0f, 0, 2.0f);
 
         //Line Tracing
         FCollisionQueryParams QueryParams;
